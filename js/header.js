7864e5fa9c7b825df8d25fc82ab687f2175dc5e2
Junior Martins <junior.soares.martins@proton.me>
2026-09-07 10:05:06 -0400

    Melhorias no Header

--- js/header.js
+++ js/header.js
@@ -13,6 +13,25 @@ document.addEventListener("DOMContentLoaded", async () => {
 		}
 
 		headerContainer.innerHTML = await response.text();
+
+		const agora = new Date();
+
+		const dataExtenso = new Intl.DateTimeFormat("pt-BR", {
+			weekday: "long", 
+			day: "numeric", 
+			month: "long", 
+			year: "numeric"
+		}).format(agora);
+
+		const horaExtenso = new Intl.DateTimeFormat("pt-BR", {
+			hour: "2-digit", 
+			minute: "2-digit"
+		}).format(agora);
+
+		document.getElementById("data-extenso").textContent = dataExtenso;
+		document.getElementById("hora-extenso").textContent = horaExtenso;
+		document.getElementById("localizacao-extenso").textContent = "Cuiabá, Mato Grosso, Brasil";
+
 	} catch (error) {
 		console.error(error);
 	}
