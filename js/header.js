843b85cb80a7e0feb0dd06828a787b214286597a
Junior Martins <junior.soares.martins@proton.me>
2026-09-22 08:45:27 -0400

    ...

--- js/header.js
+++ js/header.js
@@ -16,20 +16,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 		headerContainer.innerHTML = await response.text();
 
-		// const agora = new Date();
-
-		// const dataExtenso = new Intl.DateTimeFormat("pt-BR", {
-		// 	weekday: "long", 
-		// 	day: "numeric", 
-		// 	month: "long", 
-		// 	year: "numeric"
-		// }).format(agora);
-
-		// const horaExtenso = new Intl.DateTimeFormat("pt-BR", {
-		// 	hour: "2-digit", 
-		// 	minute: "2-digit"
-		// }).format(agora);
-
 		document.getElementById("data-extenso").textContent = getDatePorExtenso();
 		document.getElementById("hora-extenso").textContent = getTimeHM();
 		document.getElementById("localizacao-extenso").textContent = "Cuiabá, Mato Grosso, Brasil";
